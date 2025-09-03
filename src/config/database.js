7954b90b7bee95b8c2383a744517c530bb932eb2
Franac0a <fbenitez0021@gmail.com>
2025-09-02 22:19:35 -0300

    creo los archivos y estructura

---
+++ src/config/database.js
@@ -0,0 +1,26 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.HOST || "localhost",
+    dialect: process.env.DB_DIALECT || "mysql",
+  }
+);
+
+export const starDb = async () => {
+  try {
+    await sequelize.authenticate();
+
+    await sequelize.sync(); //({ force: true });
+    console.log("Se establecio conexion con la base de datos");
+  } catch (error) {
+    console.log(error);
+    console.log("Error al conectar la base de datos");
+  }
+};
